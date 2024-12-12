e1f774eeb215805e8aae71c43bac1e2befadccb1
silverkse <rlatjddmsrla@naver.com>
2024-12-12 14:41:01 +0900

    react&springboot link ok

---
+++ backend/src/main/java/com/sns/MainController.java
@@ -0,0 +1,22 @@
+package com.sns;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	
+	@GetMapping("/test")
+	public List<String> hello() {
+		List<String> test = new ArrayList<String>();
+		test.add("seoul");
+		test.add("incheon");
+		test.add("bucheon");
+		test.add("gumcheongucheong");
+		return test;
+	}
+	
+}
