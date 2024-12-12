414c8ee527a4595314e338f9780be394b8d32e33
silverkse <rlatjddmsrla@naver.com>
2024-12-13 00:46:27 +0900

    page move

--- backend/src/main/java/com/sns/MainController.java
+++ backend/src/main/java/com/sns/MainController.java
@@ -1,9 +1,12 @@
 package com.sns;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,12 +31,20 @@ public class MainController {
 	}
 	
 	@RequestMapping("/linkchk")
-	public String test(SNSDto snsDto) {
+	public ResponseEntity<Map<String, String>> test(SNSDto snsDto) {
 		System.out.println("username" + snsDto.getUsername());
 		System.out.println("age" + snsDto.getAge());
 		System.out.println("dazim" + snsDto.getDazim());
 		snsDao.mtdInsert(snsDto);
-		return "plz...";
+		Map<String, String> response = new HashMap<>();
+		response.put("redirectUrl", "/res");
+		return ResponseEntity.ok(response);
+	}
+	
+	@GetMapping("/select")
+	public List<SNSDto> select() {
+		List<SNSDto> list = snsDao.mtdSelect();
+		return list;
 	}
 	
 }
