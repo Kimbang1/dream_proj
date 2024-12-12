1c60b8587360f42166073f19bdf314139d26bb2a
silverkse <rlatjddmsrla@naver.com>
2024-12-12 18:22:19 +0900

    insert db

--- backend/src/main/java/com/sns/MainController.java
+++ backend/src/main/java/com/sns/MainController.java
@@ -3,11 +3,19 @@ package com.sns;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sns.dao.SNSDao;
+import com.sns.dto.SNSDto;
 
 @RestController
 public class MainController {
+	
+	@Autowired
+	SNSDao snsDao;
 	
 	@GetMapping("/test")
 	public List<String> hello() {
@@ -19,8 +27,12 @@ public class MainController {
 		return test;
 	}
 	
-	@GetMapping("/tester")
-	public String test() {
+	@RequestMapping("/linkchk")
+	public String test(SNSDto snsDto) {
+		System.out.println("username" + snsDto.getUsername());
+		System.out.println("age" + snsDto.getAge());
+		System.out.println("dazim" + snsDto.getDazim());
+		snsDao.mtdInsert(snsDto);
 		return "plz...";
 	}
 	
--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
   const [message, setMessage] = useState([]);
 
-  useEffect( () => {
-    fetch("test").then((response) => {
-      return response.json();
-    })
-    .then(function (data) {
-      setMessage(data);
-    });
+  useEffect(() => {
+    fetch("test")
+      .then((response) => {
+        return response.json();
+      })
+      .then(function (data) {
+        setMessage(data);
+      });
   }, []);
 
   return (
     <div>
-      <img src={logo} className='App-logo' alt="logo" />
+      <img src={logo} className="App-logo" alt="logo" />
+      <form method="post" action="/linkchk">
+        <span>이름</span>
+        <input type="text" name="username" id="username" />
+        <br />
+        <span>나이</span>
+        <input type="text" name="age" id="age" />
+        <br />
+        <span>다짐</span>
+        <input type="text" name="dazim" id="dazim" />
+        <br />
+        <button>전송</button>
+      </form>
       <ul>
-        {message.map((text, index) => <li>{index+1}번째 =&gt; {text}</li>)}
+        {message.map((text, index) => (
+          <li>
+            {index + 1}번째 =&gt; {text}
+          </li>
+        ))}
       </ul>
     </div>
   );
