4d226564d9b108efd57e8d49739ebdb0562c2126
Kimbang1 <arto135@naver.com>
2024-12-31 11:15:40 +0900

    로그아웃시 쿠키삭제 작업ver.01

--- front/src/componunts/layout/Leftaside.jsx
+++ front/src/componunts/layout/Leftaside.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import { AxiosApi } from "../../servies/AxiosApi";
 import { useMediaQuery } from "react-responsive";
+import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
 
 function Leftaside({ setIsUserMainPage }) {
   const hiddenAside = useMediaQuery({ maxWidth: 750 });
+  const [cookies, setCookie, removeCookie] = useCookies(["accessToken"]);
+
+  const handleLogout = async() => {
+
+    //로그아웃시 쿠키 삭제
+    if (window.confirm("로그아웃 하시겠습니까?")) {
+      removeCookie("accessToken", { path: "/" ,domain:"your-domain.com"});
+      removeCookie("refreshToken", { path: "/", domain: "your-domain.com" });
+      try{
+        await AxiosApi.post("auth/logout",{},{withCredentials:true});//로그아웃
+      // 쿠키 삭제 후 확인
+      if (!cookies.accessToken) {
+        console.log("쿠키 삭제 성공");
+      } else {
+        console.log("쿠키 삭제 실패");
+      }
+      //로그아웃 성공시 랜딩 페이지로 아래껄 주석 지우고 활성화 시켜주세용
+      // window.location.href = "/Landing";
+      }catch(error){
+        console.log("로그아웃 실패:",error);
+      }
+    }
+  };
+
   const navigate = useNavigate();
 
   const handlehomeClick = () => {
@@ -47,6 +73,10 @@ function Leftaside({ setIsUserMainPage }) {
                 src="/images/cat.jpg"
                 alt="프로필"
               />
+            </div>
+
+            <div className="menu">
+              <button onClick={handleLogout}>로그아웃</button>
             </div>
 
             <div className="Manager">
