0981dd35c4ffc24fdaafc03e32634533f266f495
Kimbang1 <arto135@naver.com>
2025-01-09 14:26:23 +0900

    백업ver.02

---
+++ backend/src/main/java/com/sns/ctr/ContentController.java
@@ -0,0 +1,270 @@
+package com.sns.ctr;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.sns.dao.FileListMapper;
+import com.sns.dao.FilePostMapper;
+import com.sns.dao.PostMapper;
+import com.sns.dao.UserDao;
+import com.sns.dto.FileListDto;
+import com.sns.dto.FilePostDto;
+import com.sns.dto.PostDto;
+import com.sns.dto.UserDto;
+import com.sns.jwt.JwtProvider;
+import com.sns.svc.FileService;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/contents")
+@Slf4j
+@RequiredArgsConstructor
+public class ContentController {
+   
+   private final FileService fileService;
+   private final FileListMapper fileListMapper;
+   private final JwtProvider jwtProvider;
+   private final UserDao userDao;
+   private final FilePostMapper filePostMapper;
+   private final PostMapper postMapper;
+   
+   @RequestMapping("/search")
+   public ResponseEntity<?> mtdSearch(@RequestParam String keyword) {
+      
+      return null;
+   }
+   
+   @RequestMapping("/galleryView")
+   public ResponseEntity<List<Map<String, String>>> mtdGalleryView() {
+      log.info("/galleryView까지는 왔어");
+      // 1. FilePostDto 리스트 가져오기
+      List<FilePostDto> filePostList = filePostMapper.selectAllList();
+      
+      // 2. 반환할 데이터 생성
+      List<Map<String, String>> responseList = new ArrayList<>();
+      
+      for (FilePostDto filePost : filePostList) {
+         // file_id로 FileListDto 가져오기
+         FileListDto fileData = fileListMapper.selectFileData(filePost.getFile_id());
+         
+         if(fileData != null) {
+            // file_path와 uuid를 조합 해 반환할 데이터 생성
+            Map<String, String> responseItem = new HashMap<>();
+            responseItem.put("linkId", filePost.getLink_id());
+            responseItem.put("fileName", fileData.getUp_filename());
+            responseItem.put("filePath", fileData.getFile_path());
+            
+            responseList.add(responseItem);
+         }
+      }
+      return ResponseEntity.ok(responseList);
+   }
+   
+   @RequestMapping("postView")
+   public ResponseEntity<?> mtdPostView() {
+      log.info("/postView까지는 왔어");
+      // 1. filePost 목록 가져오기
+      List<FilePostDto> filePostList = filePostMapper.selectAllList();
+      
+      // 2. 반환할 데이터 생성
+      List<Map<String, String>> responseList = new ArrayList<>();
+      
+      for (FilePostDto filePost : filePostList) {
+         // post와 file 정보 가져오기
+         FileListDto fileData = fileListMapper.selectFileData(filePost.getFile_id());
+         PostDto postData = postMapper.selectAllPost(filePost.getPost_id());
+         
+         if(fileData != null && postData != null) {
+            Map<String, String> responseItem = new HashMap<>();
+            responseItem.put("linkId", filePost.getLink_id());
+            responseItem.put("uuid", postData.getWrite_user());
+            responseItem.put("tagId", userDao.mtdSelectTagId(postData.getWrite_user()));
+            responseItem.put("filePath", fileData.getFile_path());
+            responseItem.put("content", postData.getContent());
+            
+            responseList.add(responseItem);
+         }
+      }
+      return ResponseEntity.ok(responseList);
+   }
+   
+   @PostMapping("/fileUpload")
+   public ResponseEntity<HashMap<String, String>> mtdFileUpload(
+         @RequestParam("file") MultipartFile file,
+         @RequestParam("latitude") String latitude,
+         @RequestParam("longitude") String longitude,
+         @RequestParam("captured_at") String captured_at,
+         HttpServletRequest request
+         ) {
+      // 파일 이름과 MIME 타입 확인
+        String originalFileName = file.getOriginalFilename();
+        String mimeType = file.getContentType();
+        System.out.println("original file name: " + originalFileName);
+        System.out.println("mimeType: " + mimeType);
+
+        System.out.println("captured_at: " + captured_at);
+        
+        Timestamp timestamp = Timestamp.from(Instant.parse(captured_at));
+        System.out.println("timestamp: " + timestamp);
+        
+        HashMap<String, String> responseBody = new HashMap<>();
+        
+        // AccessToken 추출
+        String accessToken = null;
+       Cookie[] cookies = request.getCookies();
+       if (cookies != null) {
+          for (Cookie cookie : cookies) {
+             if ("accessToken".equals(cookie.getName())) {
+                accessToken = cookie.getValue();
+                break;
+             }
+          }
+       }
+       
+       if (accessToken == null) {
+           log.error("Access token이 존재하지 않습니다.");
+           responseBody.put("message", "로그인이 필요합니다.");
+           return new ResponseEntity<>(responseBody, HttpStatus.UNAUTHORIZED);
+       }
+       
+       // AccessToken에서 사용자 정보 추출
+       String email = jwtProvider.getEmailFromToken(accessToken);
+       String provider = jwtProvider.getProviderFromToken(accessToken);
+       UserDto user = userDao.mtdFindByEmailAndProvider(email, provider);
+
+        // 파일이 정상적으로 업로드되었는지 확인
+        if (file.isEmpty()) {
+            responseBody.put("message", "파일이 비어 있습니다.");
+            return new ResponseEntity<>(responseBody, HttpStatus.BAD_REQUEST); // 400 Bad Request 반환
+        }
+        
+        Double doubleLong = 0.0;
+        Double doubleLati = 0.0;
+        if (longitude != null && !longitude.equals("null")) {
+           doubleLong = Double.valueOf(longitude);
+        } else {
+            // 적절한 기본값을 할당하거나, 예외를 처리하세요
+           doubleLong = 0.0; // 기본값 0.0으로 처리
+        }
+        if (latitude != null && !latitude.equals("null")) {
+           doubleLati = Double.valueOf(latitude);
+        } else {
+           // 적절한 기본값을 할당하거나, 예외를 처리하세요
+           doubleLati = 0.0; // 기본값 0.0으로 처리
+        }
+
+        try {
+           // 1. 파일 로컬에 저장
+           
+            String filePath = fileService.saveFile(file);
+            
+            // 2. DB에 파일 정보 저장
+            FileListDto fileListDto = new FileListDto();
+            String file_id = UUID.randomUUID().toString();
+            fileListDto.setFile_id(file_id);
+            fileListDto.setOri_filename(originalFileName);
+            fileListDto.setUp_filename(filePath.substring(filePath.lastIndexOf("\\") + 1));
+            fileListDto.setFile_path(filePath);
+            fileListDto.setExtension(originalFileName.substring(originalFileName.lastIndexOf(".")));
+            fileListDto.setLatitude(doubleLati);
+            fileListDto.setLongitude(doubleLong);
+            fileListDto.setCaptured_at(timestamp);
+            
+            log.info("file_id: " + fileListDto.getFile_id());
+            log.info("ori_filename: " + fileListDto.getOri_filename());
+            log.info("up_filename" + fileListDto.getUp_filename());
+            log.info("file_path" + fileListDto.getFile_path());
+            log.info("extension: " + fileListDto.getExtension());
+            log.info("latitude: " + fileListDto.getLatitude());
+            log.info("logitude: " + fileListDto.getLongitude());
+            log.info("captured_at: " + fileListDto.getCaptured_at());
+            
+            
+            fileListMapper.saveFileData(fileListDto);
+            
+            // file_post 테이블 저장
+            String link_id = UUID.randomUUID().toString();
+            filePostMapper.saveFileId(link_id, file_id);
+            
+            // 파일 업로드 성공 메시지 반환
+            HashMap<String, String> response = new HashMap<>();
+            response.put("link_id", link_id);
+            response.put("message", "파일이 성공적으로 업로드되었습니다.");
+            response.put("fileName", originalFileName); // 업로드된 파일 이름 반환
+            response.put("filePath", filePath);
+            return new ResponseEntity<>(response, HttpStatus.CREATED);
+        } catch (Exception e) {
+            // 오류 처리
+            HashMap<String, String> response = new HashMap<>();
+            response.put("message", "파일 업로드 중 오류가 발생했습니다.");
+            log.error("파일 업로드 중 오류 발생: ", e);
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR); // 500 Internal Server Error 반환
+        }
+   }
+   
+   @PostMapping("/postUpload")
+   public ResponseEntity<HashMap<String, String>> mtdPostUpload(@RequestBody HashMap<String, String> requestData, HttpServletRequest request) {
+      HashMap<String, String> responseBody = new HashMap<>();
+      String linkId = requestData.get("link_id");
+      String content = requestData.get("content");
+      
+      System.out.println("link_id: " + linkId);
+      System.out.println("content: " + content);
+      
+      String accessToken = null;
+      
+      // AccessToken 추출
+      Cookie[] cookies = request.getCookies();
+      if (cookies != null) {
+         for (Cookie cookie : cookies) {
+            if ("accessToken".equals(cookie.getName())) {
+               accessToken = cookie.getValue();
+               break;
+            }
+         }
+      }
+      
+      if (accessToken == null) {
+          log.error("Access token이 존재하지 않습니다.");
+          responseBody.put("message", "로그인이 필요합니다.");
+          return new ResponseEntity<>(responseBody, HttpStatus.UNAUTHORIZED);
+      }
+      
+      // AccessToken에서 사용자 정보 추출
+      String email = jwtProvider.getEmailFromToken(accessToken);
+      String provider = jwtProvider.getProviderFromToken(accessToken);
+      UserDto user = userDao.mtdFindByEmailAndProvider(email, provider);
+      
+      PostDto postDto = new PostDto();
+      String post_id = UUID.randomUUID().toString();
+      postDto.setPost_id(post_id);
+      postDto.setWrite_user(user.getUuid());
+      postDto.setContent(content);
+      postMapper.savePost(postDto);
+      
+      filePostMapper.addPostId(linkId, post_id);
+      
+      return null;
+   }
+   
+   
+   
+}
--- front/src/Pages/views/ContentWrite.jsx
+++ front/src/Pages/views/ContentWrite.jsx
@@ -6,11 +6,12 @@ import useImageMetadata from "../../hook/UseMetadata"; // 메타데이터 전송
 function ContentWrite() {
   const [file, setFile] = useState(null); // 사진 파일 상태
   const [content, setContent] = useState(""); // 입력란 텍스트 상태
-  const [link_id, setLinkId] = useState(null); // 업로드된 파일의 link_id 상태
+  const [link_id, setLinkId] = useState(null);
   const [posts, setPosts] = useState([]); // 게시물 리스트 상태
   const { validateImageTime, errorMessage: timeErrorMessage } =
     useImageTimeCheck(); // 시간 체크 훅 사용
-  const { handleMetadataAndSend, errorMSG } = useImageMetadata(); // 메타데이터 전송 훅 사용
+  const { handleMetadataAndSend, errorMSG, previewURL, setPreviewURL } =
+    useImageMetadata(); // 메타데이터 전송 훅 사용
   const [imagePreview, setImagePreview] = useState(null);
 
   // 컴포넌트 언마운트 시 imagePreview 삭제
@@ -23,10 +24,8 @@ function ContentWrite() {
   }, [imagePreview]);
 
   // 파일 선택 핸들러
-  const handleFileChange = async (event) => {
-    event.preventDefault();
-    const selectedFile = event.target.files[0];
-
+  const handleFileChange = async (e) => {
+    const selectedFile = e.target.files[0];
     if (!selectedFile) {
       alert("파일을 선택하지 않았습니다.");
       return;
@@ -66,32 +65,20 @@ function ContentWrite() {
       return;
     }
 
-    // 시간 체크
+    // 시간 체크 및 메타데이터 전송
     const isValidImageTime = await validateImageTime(selectedFile);
     if (!isValidImageTime) {
       alert("업로드 가능한 시간이 초과된 사진입니다.");
-      setFile(null); // 파일 상태 초기화
       return;
     }
 
-
-    // FileReader를 사용하여 미리보기 URL 생성
-    const previewURLTest = URL.createObjectURL(selectedFile);
-    // const reader = new FileReader();
-    // reader.onload = () => {
-    //   setPreviewURL(reader.result); // 로컬 미리보기 URL 설정
-    // };
-    // reader.onerror = () => {
-    //   alert("파일을 읽는 도중 문제가 발생했습니다.");
-    // };
-    // reader.readAsDataURL(selectedFile);
-
-    // 메타데이터 전송
-    const metadata = await handleMetadataAndSend(selectedFile, content);
-    if (metadata) {
-      setFile(selectedFile); // 파일 상태 업데이트
-      setLinkId(metadata.link_id); // 서버에서 반환된 link_id 저장
-      alert("파일이 성공적으로 업로드되었습니다.");
+    const isMetadataSent = await handleMetadataAndSend(selectedFile);
+    if (isMetadataSent) {
+      setFile(selectedFile);
+      // 파일 업로드 성공 후 link_id 저장
+      const linkIdFromResponse = isMetadataSent.link_id;
+      setLinkId(linkIdFromResponse);
+      console.log("파일이 성공적으로 처리되었습니다.");
     } else {
       alert("파일 처리에 실패했습니다.");
       // console.log("previewURL", previewURL); // 미리보기 URL 로그 출력
@@ -99,10 +86,13 @@ function ContentWrite() {
     }
   };
 
-  // 게시글 작성 완료 핸들러
+  const handleTextUpdate = (e) => {
+    setContent(e.target.value);
+  };
+
   const handleComplete = async () => {
-    if (!link_id || !content.trim()) {
-      alert("사진 업로드를 완료하고 내용을 입력하세요.");
+    if (!file || !content.trim()) {
+      alert("사진과 내용을 모두 입력하세요.");
       return;
     }
 
@@ -147,15 +137,16 @@ function ContentWrite() {
   return (
     <div className="box">
       {/* 사진 업로드 영역 */}
+
       <div className="uploadArea">
-        {/* 업로드된 이미지 미리보기 */}
-
-        <img
-          src={imagePreview}
-          alt="업로드된 이미지"
-          className="uploadedImage"
-        />
-
+        {/* 업로드된 이미지 영역 */}
+        {file && (
+          <img
+            src={imagePreview}
+            alt="업로드된 이미지"
+            className="uploadedImage"
+          />
+        )}
 
         {/* 파일 선택 클릭 영역 */}
         <input
@@ -181,15 +172,13 @@ function ContentWrite() {
 
       {/* 오류 메시지 */}
       {(timeErrorMessage || errorMSG) && (
-        <p style={{ color: "red" }}>
-          {timeErrorMessage || errorMSG} {/* 하나의 오류 메시지만 표시 */}
-        </p>
+        <p style={{ color: "red" }}>{timeErrorMessage || errorMSG}</p>
       )}
 
-      {/* 게시글 작성 영역 */}
+      {/* 게시글 영역 */}
       <div className="contentArea">
         <div className="completeiconArea">
-          {/* 카메라 아이콘 */}
+          {/* 카메라 아이콘을 파일 업로드와 연결 */}
           <label htmlFor="fileInput" className="cameraIcon">
             <img
               className="cameraIcon"
@@ -205,7 +194,7 @@ function ContentWrite() {
           placeholder="입력란"
           maxLength={300}
           value={content}
-          onChange={(e) => setContent(e.target.value)}
+          onChange={handleTextUpdate}
         ></textarea>
       </div>
     </div>
